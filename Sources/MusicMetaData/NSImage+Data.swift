143240364a0315c4967641d14d1ec95bbd0ab17f
Robert Cheal <rcheal52@yahoo.com>
2020-09-25 14:03:56 -0400

    Make NSImage extensions public

--- Sources/MusicMetaData/NSImage+Data.swift
+++ Sources/MusicMetaData/NSImage+Data.swift
@@ -8,14 +8,14 @@
 import Foundation
 import Cocoa
 
-extension NSBitmapImageRep {
+public extension NSBitmapImageRep {
     var png: Data? { representation(using: .png, properties: [:])}
     var jpg: Data? { representation(using: .jpeg, properties: [:])}
 }
-extension Data {
+public extension Data {
     var bitmap: NSBitmapImageRep? { NSBitmapImageRep(data: self)}
 }
-extension NSImage {
+public extension NSImage {
     var png: Data? { tiffRepresentation?.bitmap?.png}
     var jpg: Data? { tiffRepresentation?.bitmap?.jpg}
 }
