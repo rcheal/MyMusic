7e8eefd16b4fc3e5012bd50020ed692b9cbe13f5
Robert Cheal <rcheal52@yahoo.com>
2020-11-02 11:27:41 -0500

    Fix git

--- Tests/MusicMetadataTests/XCTestManifests.swift
+++ Tests/MusicMetadataTests/XCTestManifests.swift
@@ -4,5 +4,14 @@
 //
 //  Created by Robert Cheal on 11/2/20.
 //
+import XCTest
 
-import Foundation
+#if !canImport(ObjectiveC)
+public func allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(MusicMetadataFieldTests.allTests),
+        testCase(MusicMetadataSortTests.allTests),
+        testCase(MusicMetadataUpdateTests.allTests)
+    ]
+}
+#endif
