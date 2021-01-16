0822f9a8f13f19197c1feb94e9f042c78bf35a7d
Robert Cheal <rcheal52@yahoo.com>
2021-01-16 11:25:27 -0500

    Add shared: Bool to PlayList

--- Sources/MusicMetadata/Playlist.swift
+++ Sources/MusicMetadata/Playlist.swift
@@ -16,14 +16,16 @@ public struct Playlist: Identifiable, Hashable {
     public var id: String
     
     public var title: String
-        
+    public var shared: Bool
+    
     public var nextItemIndex: Int
     public var items: [PlaylistItem]
     public var randomItems: [PlaylistItem]?
     
-    init(_ title: String) {
+    init(_ title: String, shared: Bool = false) {
         id = UUID().uuidString
         self.title = title
+        self.shared = shared
         nextItemIndex = -1
         items = []
     }
@@ -39,6 +41,7 @@ extension Playlist: Codable {
     public enum CodingKeys: String, CodingKey {
         case id
         case title
+        case shared
         case nextItemIndex
         case items
     }
