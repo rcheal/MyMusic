857af6712d0171e7e373bee87a975197adda6223
Robert Cheal <rcheal52@yahoo.com>
2021-02-20 12:41:38 -0500

    Make _metadata optional on Transactions

--- Sources/MusicMetadata/APITransactions.swift
+++ Sources/MusicMetadata/APITransactions.swift
@@ -9,9 +9,9 @@ import Foundation
 
 public struct APITransactions {
     public var transactions: [Transaction]
-    public var _metadata: APIMetadata
+    public var _metadata: APIMetadata?
     
-    public init(transactions: [Transaction], _metadata: APIMetadata) {
+    public init(transactions: [Transaction], _metadata: APIMetadata? = nil) {
         self.transactions = transactions
         self._metadata = _metadata
     }
