77170d475a33c7df0bf2f84366958f57ce8df4af
rcheal <52130968+rcheal@users.noreply.github.com>
2023-02-21 17:54:15 -0500

    Add last name computed fields to Summary classes

--- Sources/MyMusic/MetadataModels/Albums/Album+computedFields.swift
+++ Sources/MyMusic/MetadataModels/Albums/Album+computedFields.swift
@@ -9,13 +9,13 @@ import Foundation
 
 extension Album {
 
-    var composerLastName: String? { lastName(composer) }
+    public var composerLastName: String? { lastName(composer) }
 
-    var artistLastName: String? { lastName(artist) }
+    public var artistLastName: String? { lastName(artist) }
 
-    var conductorLastName: String? { lastName(conductor) }
+    public var conductorLastName: String? { lastName(conductor) }
 
-    func lastName(_ name: String?) -> String? {
+    public func lastName(_ name: String?) -> String? {
         if var value = name {
             let pattern = " *\\(\\d{4}-\\d{4}\\) *"
             if let dateRange = value.range(of: pattern, options:.regularExpression) {
@@ -37,4 +37,35 @@ extension Album {
         }
         return nil
     }
+}
+
+extension AlbumSummary {
+
+    public var composerLastName: String? { lastName(composer) }
+
+    public var artistLastName: String? { lastName(artist) }
+
+    public func lastName(_ name: String?) -> String? {
+        if var value = name {
+            let pattern = " *\\(\\d{4}-\\d{4}\\) *"
+            if let dateRange = value.range(of: pattern, options:.regularExpression) {
+                value.removeSubrange(dateRange)
+            }
+            value = value.trimmingCharacters(in: .whitespaces)
+            // if person does not contain comma, strip last word and return
+            if !value.contains(",") {
+                if let index = value.lastIndex(of: " ") {
+                    let endIndex = value.endIndex
+                    let last = String(value[index..<endIndex].dropFirst())
+                    return last
+                }
+                return value
+            } else {
+                // Return everything in front of the comma
+                return value.components(separatedBy: ",")[0]
+            }
+        }
+        return nil
+    }
+
 }
