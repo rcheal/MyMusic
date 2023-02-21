eb703ead535a01217b538befc89311f107eafcc7
rcheal <52130968+rcheal@users.noreply.github.com>
2023-02-21 17:41:07 -0500

    Add artist, composer and conductor last name computed fields to Album, Composition and Single

---
+++ Sources/MyMusic/MetadataModels/Albums/Album+computedFields.swift
@@ -0,0 +1,40 @@
+//
+//  Album+computedFields.swift
+//  
+//
+//  Created by Robert Cheal on 2/21/23.
+//
+
+import Foundation
+
+extension Album {
+
+    var composerLastName: String? { lastName(composer) }
+
+    var artistLastName: String? { lastName(artist) }
+
+    var conductorLastName: String? { lastName(conductor) }
+
+    func lastName(_ name: String?) -> String? {
+        if var value = name {
+            let pattern = " *\\(\\d{4}-\\d{4}\\) *"
+            if let dateRange = value.range(of: pattern, options:.regularExpression) {
+                value.removeSubrange(dateRange)
+            }
+            value = value.trimmingCharacters(in: .whitespaces)
+            // if person does not contain comma, strip last word and return
+            if !value.contains(",") {
+                if let index = value.lastIndex(of: " ") {
+                    let endIndex = value.endIndex
+                    let last = String(value[index..<endIndex].dropFirst())
+                    return last
+                }
+                return value
+            } else {
+                // Return everything in front of the comma
+                return value.components(separatedBy: ",")[0]
+            }
+        }
+        return nil
+    }
+}
