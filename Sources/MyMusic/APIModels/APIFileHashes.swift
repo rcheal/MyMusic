8e48cbe070a66653945f3f7e360529d13f0350d5
rcheal <52130968+rcheal@users.noreply.github.com>
2023-06-04 17:48:11 -0400

    Add FileHashType to FileHash and make APIFileHashes Coddle

--- Sources/MyMusic/APIModels/APIFileHashes.swift
+++ Sources/MyMusic/APIModels/APIFileHashes.swift
@@ -7,12 +7,32 @@
 
 import Foundation
 
+public enum FileHashType: String, Codable {
+    case MD5
+    case SHA1
+    case SHA256
+    case SHA384
+    case SHA512
+}
+
+public struct FileHash: Codable {
+    public var filename: String
+    public var hash: String?
+    public var hashtype: FileHashType?
+
+    public enum CodingKeys: String, CodingKey {
+        case filename
+        case hash
+        case hashtype
+    }
+}
+
 public struct APIFileHashes {
     public var hashes: [FileHash]
 }
 
-public struct FileHash {
-    public var filename: String
-    public var hash: String?
-    public var hashtype: String?
+extension APIFileHashes: Codable {
+    public enum CodingKeys: String, CodingKey {
+        case hashes
+    }
 }
